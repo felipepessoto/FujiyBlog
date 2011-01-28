ae8373dab904b1083cfe00150b0a14e2f8022632
Fujiy <>
2011-01-27 23:55:37 -0200

    Membership

---
+++ src/FujiyBlog.Core/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using FujiyBlog.Core.DomainObjects;
+using FujiyBlog.Core.Repositories;
+using FujiyBlog.Core.Resources;
+
+namespace FujiyBlog.Core.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public CreateUserResult CreateUser(string username, string password, string email)
+        {
+            if(userRepository.EmailExistsWithAnotherUser(email, 0))
+            {
+                List<ValidationResult> validationResults = new List<ValidationResult>();
+                validationResults.Add(new ValidationResult(TextMessages.UserEmailAlreadyExists, new[] {"Email"}));
+                return new CreateUserResult();
+            }
+
+            User user = new User();
+            user.Login = username;
+            user.Email = email;
+            user.Password = password;
+
+            userRepository.Add(user);
+
+            return new CreateUserResult(user);
+        }
+    }
+}
