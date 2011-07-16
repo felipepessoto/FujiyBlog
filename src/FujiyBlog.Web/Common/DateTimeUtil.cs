2af8fe3561d5d2715f52fe9849c07891c3e29339
fujiy <none@none>
2011-07-16 19:47:14 -0300

    UTC Datetimes

--- src/FujiyBlog.Web/Common/DateTimeUtil.cs
+++ src/FujiyBlog.Web/Common/DateTimeUtil.cs
@@ -10,9 +10,23 @@ namespace FujiyBlog.Web.Common
             return TimeZoneInfo.ConvertTimeFromUtc(dateTime, Settings.SettingRepository.TimeZone);
         }
 
+        public static DateTime? ConvertUtcToMyTimeZone(DateTime? dateTime)
+        {
+            if (dateTime.HasValue)
+                return ConvertUtcToMyTimeZone(dateTime.Value);
+            return null;
+        }
+
         public static DateTime ConvertMyTimeZoneToUtc(DateTime dateTime)
         {
             return TimeZoneInfo.ConvertTimeToUtc(dateTime, Settings.SettingRepository.TimeZone);
+        }
+
+        public static DateTime? ConvertMyTimeZoneToUtc(DateTime? dateTime)
+        {
+            if (dateTime.HasValue)
+                return ConvertMyTimeZoneToUtc(dateTime.Value);
+            return null;
         }
     }
 }
