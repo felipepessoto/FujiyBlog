22f86a6ad842de605a1335f659fa9580889aa08b
fujiy <none@none>
2011-11-05 00:04:23 -0200

    Cache

--- src/FujiyBlog.Web/Controllers/FeedController.cs
+++ src/FujiyBlog.Web/Controllers/FeedController.cs
@@ -15,6 +15,7 @@ namespace FujiyBlog.Web.Controllers
             this.feedGenerator = feedGenerator;
         }
 
+        [OutputCache(Duration = 500)]
         public virtual ActionResult Rss20()
         {
             string feedUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Url.Action(MVC.Feed.Rss20());
@@ -25,6 +26,7 @@ namespace FujiyBlog.Web.Controllers
             return Content(feedGenerator.GetBlog<Rss20FeedFormatter>(feedUrl, authorUrl, postUrl));
         }
 
+        [OutputCache(Duration = 500)]
         public virtual ActionResult Atom10()
         {
             string feedUrl = Request.Url.Scheme + "://" + Request.Url.Authority + Url.Action(MVC.Feed.Rss20());
