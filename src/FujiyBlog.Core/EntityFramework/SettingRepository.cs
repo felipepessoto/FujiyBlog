0c2a029b2ae6b1433446eb89db59aa1e55c783aa
fujiy <none@none>
2011-11-12 14:51:32 -0200

    Settings Cache

--- src/FujiyBlog.Core/EntityFramework/SettingRepository.cs
+++ src/FujiyBlog.Core/EntityFramework/SettingRepository.cs
@@ -1,52 +1,55 @@
 ﻿using System;
-using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using FujiyBlog.Core.Caching;
 
 namespace FujiyBlog.Core.EntityFramework
 {
     public class SettingRepository
     {
         private readonly FujiyBlogDatabase database;
+        private readonly Dictionary<int, string> settings;
 
         public SettingRepository(FujiyBlogDatabase database)
         {
             this.database = database;
-            database.Settings.Load();
+            settings = CacheHelper.FromCacheOrExecute(() => database.Settings.ToDictionary(x => x.Id, x => x.Value), "FujiyBlog.Core.EntityFramework.SettingRepository.settings");
         }
 
         public int  MinRequiredPasswordLength
         {
-            get { return int.Parse(database.Settings.Find((int)SettingNames.MinRequiredPasswordLength).Value); }
-            set { SaveSettings(SettingNames.MinRequiredPasswordLength, value.ToString()); }
+            get { return int.Parse(LoadSetting(SettingNames.MinRequiredPasswordLength)); }
+            set { SaveSetting(SettingNames.MinRequiredPasswordLength, value.ToString()); }
         }
 
         public int PostsPerPage
         {
-            get { return int.Parse(database.Settings.Find((int)SettingNames.PostsPerPage).Value); }
-            set { SaveSettings(SettingNames.PostsPerPage, value.ToString()); }
+            get { return int.Parse(LoadSetting(SettingNames.PostsPerPage)); }
+            set { SaveSetting(SettingNames.PostsPerPage, value.ToString()); }
         }
 
         public string BlogName
         {
-            get { return database.Settings.Find((int)SettingNames.BlogName).Value; }
-            set { SaveSettings(SettingNames.BlogName, value); }
+            get { return LoadSetting(SettingNames.BlogName); }
+            set { SaveSetting(SettingNames.BlogName, value); }
         }
 
         public string BlogDescription
         {
-            get { return database.Settings.Find((int)SettingNames.BlogDescription).Value; }
-            set { SaveSettings(SettingNames.BlogDescription, value); }
+            get { return LoadSetting(SettingNames.BlogDescription); }
+            set { SaveSetting(SettingNames.BlogDescription, value); }
         }
 
         public string Theme
         {
-            get { return database.Settings.Find((int)SettingNames.Theme).Value; }
-            set { SaveSettings(SettingNames.Theme, value); }
+            get { return LoadSetting(SettingNames.Theme); }
+            set { SaveSetting(SettingNames.Theme, value); }
         }
 
         public TimeZoneInfo TimeZone
         {
-            get { return TimeZoneInfo.FindSystemTimeZoneById(database.Settings.Find((int)SettingNames.TimeZone).Value); }
-            set { SaveSettings(SettingNames.TimeZone, value.Id); }
+            get { return TimeZoneInfo.FindSystemTimeZoneById(LoadSetting(SettingNames.TimeZone)); }
+            set { SaveSetting(SettingNames.TimeZone, value.Id); }
         }
 
 
@@ -54,44 +57,44 @@ namespace FujiyBlog.Core.EntityFramework
 
         public string EmailTo
         {
-            get { return database.Settings.Find((int)SettingNames.EmailTo).Value; }
-            set { SaveSettings(SettingNames.EmailTo, value); }
+            get { return LoadSetting(SettingNames.EmailTo); }
+            set { SaveSetting(SettingNames.EmailTo, value); }
         }
 
         public string EmailSubjectPrefix
         {
-            get { return database.Settings.Find((int)SettingNames.EmailSubjectPrefix).Value; }
-            set { SaveSettings(SettingNames.EmailSubjectPrefix, value); }
+            get { return LoadSetting(SettingNames.EmailSubjectPrefix); }
+            set { SaveSetting(SettingNames.EmailSubjectPrefix, value); }
         }
 
         public string SmtpAddress
         {
-            get { return database.Settings.Find((int)SettingNames.SmtpAddress).Value; }
-            set { SaveSettings(SettingNames.SmtpAddress, value); }
+            get { return LoadSetting(SettingNames.SmtpAddress); }
+            set { SaveSetting(SettingNames.SmtpAddress, value); }
         }
 
         public int SmtpPort
         {
-            get { return int.Parse(database.Settings.Find((int)SettingNames.SmtpPort).Value); }
-            set { SaveSettings(SettingNames.SmtpPort, value.ToString()); }
+            get { return int.Parse(LoadSetting(SettingNames.SmtpPort)); }
+            set { SaveSetting(SettingNames.SmtpPort, value.ToString()); }
         }
 
         public string SmtpUserName
         {
-            get { return database.Settings.Find((int)SettingNames.SmtpUserName).Value; }
-            set { SaveSettings(SettingNames.SmtpUserName, value); }
+            get { return LoadSetting(SettingNames.SmtpUserName); }
+            set { SaveSetting(SettingNames.SmtpUserName, value); }
         }
 
         public string SmtpPassword
         {
-            get { return database.Settings.Find((int)SettingNames.SmtpPassword).Value; }
-            set { SaveSettings(SettingNames.SmtpPassword, value); }
+            get { return LoadSetting(SettingNames.SmtpPassword); }
+            set { SaveSetting(SettingNames.SmtpPassword, value); }
         }
 
         public bool SmtpSsl
         {
-            get { return bool.Parse(database.Settings.Find((int)SettingNames.SmtpSsl).Value); }
-            set { SaveSettings(SettingNames.SmtpSsl, value.ToString()); }
+            get { return bool.Parse(LoadSetting(SettingNames.SmtpSsl)); }
+            set { SaveSetting(SettingNames.SmtpSsl, value.ToString()); }
         }
 
 
@@ -99,50 +102,50 @@ namespace FujiyBlog.Core.EntityFramework
 
         public bool EnableComments
         {
-            get { return bool.Parse(database.Settings.Find((int)SettingNames.EnableComments).Value); }
-            set { SaveSettings(SettingNames.EnableComments, value.ToString()); }
+            get { return bool.Parse(LoadSetting(SettingNames.EnableComments)); }
+            set { SaveSetting(SettingNames.EnableComments, value.ToString()); }
         }
 
         public bool ModerateComments
         {
-            get { return bool.Parse(database.Settings.Find((int)SettingNames.ModerateComments).Value); }
-            set { SaveSettings(SettingNames.ModerateComments, value.ToString()); }
+            get { return bool.Parse(LoadSetting(SettingNames.ModerateComments)); }
+            set { SaveSetting(SettingNames.ModerateComments, value.ToString()); }
         }
 
         public bool EnableNestedComments
         {
-            get { return bool.Parse(database.Settings.Find((int)SettingNames.EnableNestedComments).Value); }
-            set { SaveSettings(SettingNames.EnableNestedComments, value.ToString()); }
+            get { return bool.Parse(LoadSetting(SettingNames.EnableNestedComments)); }
+            set { SaveSetting(SettingNames.EnableNestedComments, value.ToString()); }
         }
 
         public int? CloseCommentsAfterDays
         {
             get
             {
-                string value = database.Settings.Find((int)SettingNames.CloseCommentsAfterDays).Value;
+                string value = LoadSetting(SettingNames.CloseCommentsAfterDays);
                 if (value == null)
                     return null;
                 return int.Parse(value);
             }
-            set { SaveSettings(SettingNames.CloseCommentsAfterDays, value.HasValue ? value.ToString() : null); }
+            set { SaveSetting(SettingNames.CloseCommentsAfterDays, value.HasValue ? value.ToString() : null); }
         }
 
         public int CommentsPerPage
         {
-            get { return int.Parse(database.Settings.Find((int)SettingNames.CommentsPerPage).Value); }
-            set { SaveSettings(SettingNames.CommentsPerPage, value.ToString()); }
+            get { return int.Parse(LoadSetting(SettingNames.CommentsPerPage)); }
+            set { SaveSetting(SettingNames.CommentsPerPage, value.ToString()); }
         }
 
         public string CommentsAvatar
         {
-            get { return database.Settings.Find((int)SettingNames.CommentsAvatar).Value; }
-            set { SaveSettings(SettingNames.CommentsAvatar, value); }
+            get { return LoadSetting(SettingNames.CommentsAvatar); }
+            set { SaveSetting(SettingNames.CommentsAvatar, value); }
         }
 
         public string Culture
         {
-            get { return database.Settings.Find((int)SettingNames.Culture).Value; }
-            set { SaveSettings(SettingNames.Culture, value); }
+            get { return LoadSetting(SettingNames.Culture); }
+            set { SaveSetting(SettingNames.Culture, value); }
         }
 
 
@@ -150,33 +153,38 @@ namespace FujiyBlog.Core.EntityFramework
 
         public bool EnableFacebookLikePosts
         {
-            get { return bool.Parse(database.Settings.Find((int)SettingNames.EnableFacebookLikePosts).Value); }
-            set { SaveSettings(SettingNames.EnableFacebookLikePosts, value.ToString()); }
+            get { return bool.Parse(LoadSetting(SettingNames.EnableFacebookLikePosts)); }
+            set { SaveSetting(SettingNames.EnableFacebookLikePosts, value.ToString()); }
         }
 
         public bool EnableGooglePlusOnePosts
         {
-            get { return bool.Parse(database.Settings.Find((int)SettingNames.EnableGooglePlusOnePosts).Value); }
-            set { SaveSettings(SettingNames.EnableGooglePlusOnePosts, value.ToString()); }
+            get { return bool.Parse(LoadSetting(SettingNames.EnableGooglePlusOnePosts)); }
+            set { SaveSetting(SettingNames.EnableGooglePlusOnePosts, value.ToString()); }
         }
 
         public bool EnableTwitterSharePosts
         {
-            get { return bool.Parse(database.Settings.Find((int)SettingNames.EnableTwitterSharePosts).Value); }
-            set { SaveSettings(SettingNames.EnableTwitterSharePosts, value.ToString()); }
+            get { return bool.Parse(LoadSetting(SettingNames.EnableTwitterSharePosts)); }
+            set { SaveSetting(SettingNames.EnableTwitterSharePosts, value.ToString()); }
         }
 
 
 
-        public long LastDatabaseChange
+        //public long LastDatabaseChange
+        //{
+        //    get { return long.Parse(LoadSetting(SettingNames.LastDatabaseChange)); }
+        //    set { SaveSetting(SettingNames.LastDatabaseChange, value.ToString()); }
+        //}
+
+        private string LoadSetting(SettingNames setting)
         {
-            get { return long.Parse(database.Settings.Find((int)SettingNames.LastDatabaseChange).Value); }
-            set { SaveSettings(SettingNames.LastDatabaseChange, value.ToString()); }
+            return settings[(int) setting];
         }
 
-        private void SaveSettings(SettingNames settings, string value)
+        private void SaveSetting(SettingNames setting, string value)
         {
-            database.Settings.Find((int) settings).Value = value;
+            database.Settings.Find((int) setting).Value = value;
             database.SaveChanges();
         }
 
