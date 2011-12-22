f166eb6a3a39daff1a14d3967347de11fe263068
fujiy <none@none>
2011-12-21 22:18:10 -0200

    CompressFilter

--- src/FujiyBlog.Web/Controllers/JavaScriptController.cs
+++ src/FujiyBlog.Web/Controllers/JavaScriptController.cs
@@ -1,11 +1,7 @@
-﻿using System.IO.Compression;
+﻿using System;
 using System.Linq;
-using System.Text;
-using System.Web;
 using System.Web.Mvc;
-using System.IO;
-using System;
-using System.Web.UI;
+using FujiyBlog.Web.Infrastructure;
 
 namespace FujiyBlog.Web.Controllers
 {
@@ -37,38 +33,6 @@ namespace FujiyBlog.Web.Controllers
             ViewBag.Bundle = bundleContent;
 
             return View();
-        }
-
-        public class CompressFilter : ActionFilterAttribute
-        {
-            public override void OnResultExecuted(ResultExecutedContext filterContext)
-            {
-                if (filterContext.Exception != null)
-                {
-                    return;
-                }
-
-                HttpRequestBase request = filterContext.HttpContext.Request;
-
-                string acceptEncoding = request.Headers["Accept-Encoding"];
-
-                if (string.IsNullOrEmpty(acceptEncoding)) return;
-
-                acceptEncoding = acceptEncoding.ToUpperInvariant();
-
-                HttpResponseBase response = filterContext.HttpContext.Response;
-
-                if (acceptEncoding.Contains("GZIP"))
-                {
-                    response.AppendHeader("Content-encoding", "gzip");
-                    response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
-                }
-                else if (acceptEncoding.Contains("DEFLATE"))
-                {
-                    response.AppendHeader("Content-encoding", "deflate");
-                    response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
-                }
-            }
         }
     }
 }
