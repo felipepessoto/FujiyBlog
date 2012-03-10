6d24e0a701210f30001c845a14e28bc929ce0350
fujiy <none@none>
2012-03-10 19:56:10 -0300

    Entity Framework Migrations

---
+++ src/FujiyBlog.Core/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace FujiyBlog.Core.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FujiyBlog.Core.EntityFramework.FujiyBlogDatabase>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FujiyBlog.Core.EntityFramework.FujiyBlogDatabase context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
