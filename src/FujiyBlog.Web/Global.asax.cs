c069a8daf2c5d1a0534f3a5517eeda3543e060df
fujiy@Fujiy-Notebook <fujiy@Fujiy-Notebook>
2012-08-28 22:56:29 -0300

    ASP.NET MVC 4

--- src/FujiyBlog.Web/Global.asax.cs
+++ src/FujiyBlog.Web/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using FujiyBlog.Core.EntityFramework;
 using FujiyBlog.Web.Infrastructure;
@@ -12,52 +14,16 @@ namespace FujiyBlog.Web
 {
     public class MvcApplication : HttpApplication
     {
-        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new ProfilingAttribute());
-            filters.Add(new HandleErrorAttribute());
-            filters.Add(new SetCultureAttribute());
-            filters.Add(new ThemeAttribute());
-        }
-
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute("PostDetailId", "postid/{Id}", MVC.Post.DetailsById(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("PostDetail", "post/{*PostSlug}", MVC.Post.Details(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("PageById", "pageid/{Id}", MVC.Page.DetailsById(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("Page", "page/{*PageSlug}", MVC.Page.Details(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("TagHome", "tag/{tag}", MVC.Post.Tag(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("CategoryHome", "category/{category}", MVC.Post.Category(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("AuthorHome", "author/{author}", MVC.Post.Author(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("Archive", "archive", MVC.Post.Archive(), new[] { "FujiyBlog.Web.Controllers" });
-            routes.MapRoute("ArchiveByMonth", "archive/{year}/{month}", MVC.Post.ArchiveDate(), null, new { year = @"\d{4}", month = @"\d{1,2}" }, new[] { "FujiyBlog.Web.Controllers" });
-
-            routes.MapRoute(
-                "HomePosts",
-                "",
-                new { controller = "Post", action = "Index" },
-                new { controller = new HomeConstraint() },
-                new[] { "FujiyBlog.Web.Controllers" });
-
-            routes.MapRoute("BlogHome", "blog", new { controller = "Post", action = "Index" }, new[] { "FujiyBlog.Web.Controllers" });
-
-            routes.MapRoute(
-                "DefaultPage",
-                "",
-                new {controller = "Page", action = "Index"},
-                new[] {"FujiyBlog.Web.Controllers"});
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "", action = "", id = UrlParameter.Optional }, // Parameter defaults
-                new[] { "FujiyBlog.Web.Controllers" });
-        }
-
         protected void Application_Start()
         {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
             //AutoDbMigration();
 
             LogManager.GetCurrentClassLogger().Info("Started FujiyBlog");
@@ -66,11 +32,6 @@ namespace FujiyBlog.Web
             {
                 ViewEngines.Engines.Remove(viewEngine);
             }
-
-            AreaRegistration.RegisterAllAreas();
-
-            RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
 
             DependencyResolver.SetResolver(new UnityDependencyResolver());
 
