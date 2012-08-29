aae4cd191a4598fe314cd0f4ed94aacd1deeee36
fujiy <felipepessoto@gmail.com>
2012-08-28 22:56:56 -0300

    ASP.NET MVC 4

---
+++ src/FujiyBlog.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,45 @@
+﻿using FujiyBlog.Web.Infrastructure;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FujiyBlog.Web
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("PostDetailId", "postid/{Id}", MVC.Post.DetailsById(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("PostDetail", "post/{*PostSlug}", MVC.Post.Details(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("PageById", "pageid/{Id}", MVC.Page.DetailsById(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("Page", "page/{*PageSlug}", MVC.Page.Details(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("TagHome", "tag/{tag}", MVC.Post.Tag(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("CategoryHome", "category/{category}", MVC.Post.Category(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("AuthorHome", "author/{author}", MVC.Post.Author(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("Archive", "archive", MVC.Post.Archive(), new[] { "FujiyBlog.Web.Controllers" });
+            routes.MapRoute("ArchiveByMonth", "archive/{year}/{month}", MVC.Post.ArchiveDate(), null, new { year = @"\d{4}", month = @"\d{1,2}" }, new[] { "FujiyBlog.Web.Controllers" });
+
+            routes.MapRoute(
+                "HomePosts",
+                "",
+                new { controller = "Post", action = "Index" },
+                new { controller = new HomeConstraint() },
+                new[] { "FujiyBlog.Web.Controllers" });
+
+            routes.MapRoute("BlogHome", "blog", new { controller = "Post", action = "Index" }, new[] { "FujiyBlog.Web.Controllers" });
+
+            routes.MapRoute(
+                "DefaultPage",
+                "",
+                new { controller = "Page", action = "Index" },
+                new[] { "FujiyBlog.Web.Controllers" });
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "", action = "", id = UrlParameter.Optional }, // Parameter defaults
+                new[] { "FujiyBlog.Web.Controllers" });
+        }
+    }
+}
