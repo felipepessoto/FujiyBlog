3ec817ff6d38dfc055158d2c5496f2ea2dcf709e
fujiy@Fujiy-Notebook <fujiy@Fujiy-Notebook>
2012-08-28 22:56:56 -0300

    ASP.NET MVC 4

---
+++ src/FujiyBlog.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using FujiyBlog.Web.Infrastructure;
+
+namespace FujiyBlog.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new ProfilingAttribute());
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new SetCultureAttribute());
+            filters.Add(new ThemeAttribute());
+        }
+    }
+}
