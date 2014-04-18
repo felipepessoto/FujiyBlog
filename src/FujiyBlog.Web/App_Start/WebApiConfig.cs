2773e2e7b541f827ff82ae9f76b75c174cde78c2
Fujiy <felipepessoto@gmail.com>
2014-04-18 14:31:20 -0300

    Update to reflect new ASP.NET MVC 5 Template

--- src/FujiyBlog.Web/App_Start/WebApiConfig.cs
+++ src/FujiyBlog.Web/App_Start/WebApiConfig.cs
@@ -9,6 +9,11 @@ namespace FujiyBlog.Web
     {
         public static void Register(HttpConfiguration config)
         {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
