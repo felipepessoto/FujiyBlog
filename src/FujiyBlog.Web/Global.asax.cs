af96a113ec48782eb0dcdcf517f352e10d7a06fa
Fujiy <felipepessoto@gmail.com>
2014-04-18 01:55:52 -0300

    Update ASP.NET Packages

--- src/FujiyBlog.Web/Global.asax.cs
+++ src/FujiyBlog.Web/Global.asax.cs
@@ -19,7 +19,7 @@ namespace FujiyBlog.Web
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
