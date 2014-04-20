44d81a0ab4f082ab83215e3aa828d69948691837
Fujiy <felipepessoto@gmail.com>
2014-04-20 20:07:21 -0300

    Social Share button now use simple links

--- src/FujiyBlog.Web/Scripts/fujiyblog.js
+++ src/FujiyBlog.Web/Scripts/fujiyblog.js
@@ -130,7 +130,12 @@
             $('#comment-user-data').show();
             $('#logout-social-id').hide();
         }
-    }
+    },
+    windowOpenCenter: function(url, title, w, h) {
+        var left = (screen.width/2)-(w/2);
+        var top = (screen.height/2)-(h/2);
+        return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+    } 
 };
 
 $(function () {
