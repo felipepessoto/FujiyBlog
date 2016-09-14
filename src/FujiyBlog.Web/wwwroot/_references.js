81ef8e02a4fbe8494c3d616da8e8a6c294fa4355
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2016-09-14 10:35:07 -0300

    Fix TinyMCE line break Add Code Sample Update TinyMCE

--- src/FujiyBlog.Web/wwwroot/_references.js
+++ src/FujiyBlog.Web/wwwroot/_references.js
@@ -8,3 +8,4 @@
 /// <reference path="lib/jquery-ui/jquery-ui.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
+/// <reference path="lib/prism/prism.js" />
