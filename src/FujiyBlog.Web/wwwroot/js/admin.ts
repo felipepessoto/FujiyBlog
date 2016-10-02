7621de380ac6744bf7a6be27850fdf88d30bd91d
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2016-10-01 23:01:35 -0300

    Notify Bar

--- src/FujiyBlog.Web/wwwroot/js/admin.ts
+++ src/FujiyBlog.Web/wwwroot/js/admin.ts
@@ -1,12 +1,7 @@
-﻿/// <reference path="jquery.d.ts" />
-$(function () {
+﻿$(function () {
     $('.datetime-picker').datetimepicker({
         format: 'YYYY/MM/DD HH:mm'
     });
 
-    $("#notify-bar").fadeIn(2000);
-    $("#notify-bar").delay(5000).fadeOut(1000);
-    $("#close-notify").click(function () {
-        $("#notify-bar").stop(true).fadeOut('slow');
-    });
+    $("#notify-bar").slideDown(500).delay(5000).slideUp(500);
 });
