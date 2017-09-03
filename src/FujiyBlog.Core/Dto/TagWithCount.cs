9ef514a58da19ac83307cc107bde6cf67edca213
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2017-09-03 13:10:20 -0300

    Remove Tag Cloud N+1 queries

--- src/FujiyBlog.Core/Dto/TagWithCount.cs
+++ src/FujiyBlog.Core/Dto/TagWithCount.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using FujiyBlog.Core.DomainObjects;
-
-namespace FujiyBlog.Core.Dto
+﻿namespace FujiyBlog.Core.Dto
 {
     public class TagWithCount
     {
-        public Tag Tag { get; set; }
+        public string TagName { get; set; }
         public int PostsCount { get; set; }
     }
 }
